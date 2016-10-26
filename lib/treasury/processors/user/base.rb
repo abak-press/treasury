892602594c3cf4023f198b5acbfab6ffd6cc13a7
Semyon Pupkov <mail@semyonpupkov.com>
2016-10-26 15:01:10 +0500

    feature: add methods to user processor form project
    Зачем это надо:
    Получается не верное наследование классов
    Processor::Base наследуеться от проекта
    Processor::User::Base насоелдуется от проекта, а должен от Treasury::Base
    и получается теряеться метод object_value который есть только в геме
    https://github.com/abak-press/treasury/blob/master/lib/treasury/processors/base.rb#L12

--- lib/treasury/processors/user/base.rb
+++ lib/treasury/processors/user/base.rb
@@ -3,7 +3,20 @@
 module Treasury
   module Processors
     module User
-      class Base < ::CoreDenormalization::Processors::User::Base
+      class Base < Treasury::Processors::Base
+        alias :user_id= :object=
+        alias :user_id  :object
+
+        protected
+
+        def init_event_params
+          self.user_id = extract_user
+          raise ArgumentError, "User ID expected to be Integer, #{@event.inspect}" unless user_id
+        end
+
+        def extract_user
+          @event.raw_data[:user_id] || @event.raw_data[:id]
+        end
       end
     end
   end
