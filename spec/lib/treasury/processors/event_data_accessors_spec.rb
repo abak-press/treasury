c6840740cc91edb681371d1bc71ed5e0f0d59693
Salahutdinov Dmitry <dsalahutdinov@gmail.com>
2016-12-12 10:18:15 +0500

    f: bla_bla_changed? tests

--- spec/lib/treasury/processors/event_data_accessors_spec.rb
+++ spec/lib/treasury/processors/event_data_accessors_spec.rb
@@ -7,7 +7,6 @@ RSpec.describe ::Treasury::Processors::EventDataAccessors do
 
   describe '.event_data_fields' do
     context 'fast_parsing turned off' do
-      let(:event_data) { double(data: {value: :new}, prev_data: {value: :old}) }
       let(:processor_class) do
         Class.new(::Treasury::Processors::Base) do
           include ::Treasury::Processors::EventDataAccessors
@@ -16,17 +15,32 @@ RSpec.describe ::Treasury::Processors::EventDataAccessors do
         end
       end
 
-      it do
-        expect(processor.value).to eq :new
-        expect(processor.prev_value).to eq :old
-        expect(processor.value_changed?).to be_truthy
+      context 'value was changed' do
+        let(:event_data) do
+          double(data: {value: :new}, prev_data: {value: :old})
+        end
+
+        it do
+          expect(processor.value).to eq :new
+          expect(processor.prev_value).to eq :old
+          expect(processor.value_changed?).to be_truthy
+        end
+      end
+
+      context 'value was not changed' do
+        let(:event_data) do
+          double(data: {value: :old}, prev_data: {value: :old})
+        end
+
+        it do
+          expect(processor.value).to eq :old
+          expect(processor.prev_value).to eq :old
+          expect(processor.value_changed?).to be_falsey
+        end
       end
     end
 
     context 'fast_parsing turned on' do
-      let(:event_data) do
-        double(raw_data: {value: :new}, raw_prev_data: {value: :old})
-      end
       let(:processor_class) do
         Class.new(::Treasury::Processors::Base) do
           include ::Treasury::Processors::EventDataAccessors
@@ -35,10 +49,28 @@ RSpec.describe ::Treasury::Processors::EventDataAccessors do
         end
       end
 
-      it do
-        expect(processor.value).to eq :new
-        expect(processor.prev_value).to eq :old
-        expect(processor.value_changed?).to be_truthy
+      context 'value was changed' do
+        let(:event_data) do
+          double(raw_data: {value: :new}, raw_prev_data: {value: :old})
+        end
+
+        it do
+          expect(processor.value).to eq :new
+          expect(processor.prev_value).to eq :old
+          expect(processor.value_changed?).to be_truthy
+        end
+      end
+
+      context 'value was not changed' do
+        let(:event_data) do
+          double(raw_data: {value: :old}, raw_prev_data: {value: :old})
+        end
+
+        it do
+          expect(processor.value).to eq :old
+          expect(processor.prev_value).to eq :old
+          expect(processor.value_changed?).to be_falsey
+        end
       end
     end
   end
