6781bafa0dbddd5feb14d31a383d1e7486d4f97c
Sergey Kucher <s.e.kucher@gmail.com>
2017-11-02 19:57:10 +0500

    feature: ability to use multiple events loggers
    https://jira.railsc.ru/browse/ORDERS-967

--- lib/treasury/configuration.rb
+++ lib/treasury/configuration.rb
@@ -6,12 +6,14 @@ module Treasury
                   :bge_namespace,
                   :bge_queue_timeout,
                   :bge_daemon_options,
-                  :job_error_notifications
+                  :job_error_notifications,
+                  :events_loggers
 
     def initialize
       self.bge_concurrency = 4
       self.bge_queue_timeout = 300
       self.bge_max_tries_on_fail = 5
+      self.events_loggers = ['Treasury::Services::EventsLogger']
     end
   end
 end
